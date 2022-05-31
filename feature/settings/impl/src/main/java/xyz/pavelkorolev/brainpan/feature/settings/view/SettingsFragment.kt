6f29e463f592829093075c0d71c79e92da1926dd
Pavel Korolev <pavelkorolevxyz@gmail.com>
2022-05-31 19:10:53 +0400

    Removed unused code

--- feature/settings/impl/src/main/java/xyz/pavelkorolev/brainpan/feature/settings/view/SettingsFragment.kt
+++ feature/settings/impl/src/main/java/xyz/pavelkorolev/brainpan/feature/settings/view/SettingsFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import xyz.pavelkorolev.brainpan.core.compose.fragmentInsetsComposeView
 import xyz.pavelkorolev.brainpan.core.extensions.lazyFast
@@ -16,7 +15,6 @@ import xyz.pavelkorolev.brainpan.core.ui.BaseFragment
 import xyz.pavelkorolev.brainpan.feature.settings.di.DaggerSettingsComponent
 import xyz.pavelkorolev.brainpan.feature.settings.di.SettingsComponent
 import xyz.pavelkorolev.brainpan.feature.settings.di.SettingsFeatureDeps
-import kotlin.reflect.KProperty
 
 class SettingsFragment : BaseFragment() {
 
@@ -57,16 +55,5 @@ class SettingsFragment : BaseFragment() {
 
     companion object {
         fun newInstance(): Fragment = SettingsFragment()
-    }
-}
-
-class FragmentExitDelegate {
-
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return "$thisRef, thank you for delegating '${property.name}' to me!"
-    }
-
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        println("$value has been assigned to '${property.name}' in $thisRef.")
     }
 }
