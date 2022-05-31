7269efda08426b162fdf56ca6175d5815d35a029
Pavel Korolev <pavelkorolevxyz@gmail.com>
2022-05-31 16:53:56 +0400

    Fixed detekt report merging logic

--- build-logic/checks/src/main/kotlin/convention.detekt.gradle.kts
+++ build-logic/checks/src/main/kotlin/convention.detekt.gradle.kts
@@ -14,7 +14,12 @@ dependencies {
 val configFile = files("$rootDir/config/detekt/config.yml")
 val baselineFile = file("config/detekt/baseline.xml")
 val mergedReportFile = file("${rootProject.buildDir}/reports/detekt/report.xml")
-val xmlReportFile = file("$buildDir/reports/detekt/detekt.xml")
+
+/**
+ * Must be named detekt.xml
+ * Workaround for https://github.com/detekt/detekt/issues/4192#issuecomment-946325201
+ */
+val outputReportFile = file("$buildDir/reports/detekt/detekt.xml")
 
 detekt {
     ignoreFailures = true
@@ -27,26 +32,42 @@ detekt {
     baseline = baselineFile
 }
 
-val reportMerge by tasks.registering(ReportMergeTask::class) {
+val reportMerge: TaskProvider<ReportMergeTask> = rootProject.registerMaybe("reportMerge") {
     description = "Runs merge of all detekt reports into single one"
     output.set(mergedReportFile)
 }
 
-tasks.withType(Detekt::class).configureEach {
+tasks.withType<Detekt>().configureEach {
     reports {
         html.required.set(false)
         sarif.required.set(false)
         txt.required.set(false)
         xml.required.set(true)
-        xml.outputLocation.set(xmlReportFile)
+        xml.outputLocation.set(outputReportFile)
     }
 }
 
-plugins.withType(DetektPlugin::class) {
-    tasks.withType(Detekt::class) {
+plugins.withType<DetektPlugin> {
+    tasks.withType<Detekt> {
         finalizedBy(reportMerge)
         reportMerge.configure {
             input.from(xmlReportFile)
         }
     }
+}
+
+/**
+ * Workaround to get [TaskProvider] by task name if it already exists in given [Project]
+ * or register it otherwise
+ *
+ * [Github - Introduce TaskContainer.maybeNamed](https://github.com/gradle/gradle/issues/8057)
+ */
+inline fun <reified T : Task> Project.registerMaybe(
+    taskName: String,
+    configuration: Action<in T> = Action {},
+): TaskProvider<T> {
+    if (taskName in tasks.names) {
+        return tasks.named(taskName, T::class, configuration)
+    }
+    return tasks.register(taskName, T::class, configuration)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.library") version "7.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.6.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.20.0" apply false
     id("com.google.gms.google-services") version "4.3.10" apply false
     id("com.google.firebase.crashlytics") version "2.8.1" apply false
 }
